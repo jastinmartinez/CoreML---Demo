b3401bd1007549728eda48bf47abcdd95bb3ab64
Jastin Martinez <36866323+jastinmartinez@users.noreply.github.com>
2021-05-14 21:58:56 -0400

    init

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Jastin on 10/5/21.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
